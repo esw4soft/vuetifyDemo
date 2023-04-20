9b3d13279a6fd37e3507062f651199410a9780d8
esmacbook <es250528@gmail.com>
2023-04-20 23:30:05 +0800

    feat: login功能

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-content>
+    <v-main>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
@@ -10,19 +10,41 @@
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field name="email" label="Email" type="email"></v-text-field>
-                  <v-text-field id="password" name="password" label="Password"
+                  <v-text-field name="email" label="Email" type="email" v-model="user.username"></v-text-field>
+                  <v-text-field id="password" name="password" label="Password" v-model="user.password"
                     type="password"></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="primary" to="/">Login</v-btn>
+                <v-btn @click.prevent="clickSignin()" color="primary">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
         </v-layout>
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    clickSignin () {
+      // console.log(this.user.username, this.user.password)
+      const signinApi = `${process.env.VUE_APP_API}admin/signin`
+      this.$http.post(signinApi, this.user).then((res) => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
