dab1b58907b3c178ae7d79f3d847a5e8cbf63b84
esmacbook <es250528@gmail.com>
2023-04-28 21:37:35 +0800

    feat: router dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -7,8 +7,17 @@
 <script>
 export default {
   created () {
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1')
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*=\s*([^;]*).*$)|^.*$/, '$1')
     console.log(token)
+    this.$http.defaults.headers.common.Authorization = token
+
+    const signinApi = `${process.env.VUE_APP_API}api/user/check`
+    this.$http.post(signinApi, this.user).then((res) => {
+      if (!res.data.success) {
+        this.$router.push('login')
+      }
+      console.log(res)
+    })
   }
 }
 </script>
