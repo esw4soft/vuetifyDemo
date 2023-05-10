ee5c330ec927673eedaad924ed64e02830bc7e86
easonwang <es250528@gmail.com>
2023-05-10 17:12:39 +0800

    feat: 產品列表

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,336 @@
 <template>
-  <div>
-    產品列表
-  </div>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    sort-by="price"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>產品列表</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              新增產品
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.category"
+                      label="分類"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="產品名稱"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.price"
+                      label="price"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.countPrice"
+                      label="countPrice"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.open"
+                      label="open"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: '分類',
+        align: 'start',
+        sortable: false,
+        value: 'category'
+      },
+      {
+        text: '產品名稱',
+        sortable: false,
+        value: 'name'
+      },
+      { text: '原價', value: 'price' },
+      { text: '售價', value: 'countPrice' },
+      {
+        text: '是否啟用',
+        sortable: false,
+        value: 'open'
+      },
+      { text: '編輯', value: 'actions', sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      category: '',
+      name: '',
+      price: 0,
+      countPrice: 0,
+      open: true
+    },
+    defaultItem: {
+      category: '',
+      name: '',
+      price: 0,
+      countPrice: 0,
+      open: true
+    }
+  }),
+
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    }
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    }
+  },
+
+  created () {
+    this.initialize()
+  },
+
+  methods: {
+    initialize () {
+      this.desserts = [
+        {
+          category: 'as',
+          name: 'Frozen Yogurt',
+          price: 159,
+          countPrice: 6.0,
+          open: true
+        },
+        {
+          category: 'asd',
+          name: 'Ice cream sandwich',
+          price: 1519,
+          countPrice: 6.20,
+          open: true
+        },
+        {
+          category: 'asdd',
+          name: 'Eclair',
+          price: 1259,
+          countPrice: 26.0,
+          open: true
+        },
+        {
+          category: 'asdasa',
+          name: 'Cupcake',
+          price: 2159,
+          countPrice: 236.0,
+          open: true
+        },
+        {
+          category: 'zxcv',
+          name: 'Gingerbread',
+          price: 1549,
+          countPrice: 64.0,
+          open: true
+        },
+        {
+          category: 'tajjy',
+          name: 'Jelly bean',
+          price: 12159,
+          countPrice: 126.0,
+          open: true
+        },
+        {
+          category: 'as11d',
+          name: 'Lollipop',
+          price: 11219,
+          countPrice: 126.0,
+          open: true
+        },
+        {
+          category: 'asd56',
+          name: 'Honeycomb',
+          price: 15912,
+          countPrice: 62.0,
+          open: true
+        },
+        {
+          category: 'tdf',
+          name: 'Donut',
+          price: 7159,
+          countPrice: 56.0,
+          open: true
+        },
+        {
+          category: 'asd99',
+          name: 'KitKat',
+          price: 59,
+          countPrice: 67.0,
+          open: true
+        }
+      ]
+    },
+
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm () {
+      this.desserts.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
+    }
+  }
+}
+</script>
